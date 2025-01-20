dfe9c986bdf10d6483e419bc7875d285cf02afa4
Ciro <162831871+CiroFiniello@users.noreply.github.com>
2025-01-20 16:08:42 +0000

    Per l'implementazione di CustomersListQuery.cs , ho utilizzato la stessa logica implementata in suppliers, con l'aggiunta di una join per i dati derivanti dalle tabelle CustomerCategory e Customer, e poi ho aggiunto i controlli per i filtri su Name e Email, come da traccia

--- src/Backend/Features/Customers/CustomersListQuery.cs
+++ src/Backend/Features/Customers/CustomersListQuery.cs
@@ -1,7 +1,6 @@
 using MediatR;
 namespace Backend.Features.Customers;
 
-<<<<<<< Updated upstream
 public class CustomersListQuery : IRequest<List<CustomerDto>>
 {
     public string? Name { get; set; }
@@ -28,73 +27,27 @@ public class CustomersListQueryHandler : IRequestHandler<CustomersListQuery, Lis
             query = query.Where(c => c.Email.Contains(request.Email));
 
         return await query.Select(c => new CustomerDto
-{
-    Id = c.Id,
-    Name = c.Name,
-    Address = c.Address,
-    Email = c.Email,
-    Phone = c.Phone,
-    Iban = c.Iban,
-    CategoryCode = c.CustomerCategory != null ? c.CustomerCategory.Code : string.Empty,
-    CategoryDescription = c.CustomerCategory != null ? c.CustomerCategory.Description : string.Empty
-}).ToListAsync(cancellationToken);
+        {
+            Id = c.Id,
+            Name = c.Name,
+            Address = c.Address,
+            Email = c.Email,
+            Phone = c.Phone,
+            Iban = c.Iban,
+            CategoryCode = c.CustomerCategory != null ? c.CustomerCategory.Code : string.Empty,
+            CategoryDescription = c.CustomerCategory != null ? c.CustomerCategory.Description : string.Empty
+        }).ToListAsync(cancellationToken);
     }
 }
 
 public class CustomerDto
-=======
-public class Customer
->>>>>>> Stashed changes
 {
     public int Id { get; set; }
-    public string Name { get; set; } = "";
-    public string Address { get; set; } = "";
-    public string Email { get; set; } = "";
-    public string Phone { get; set; } = "";
-    public string Iban { get; set; } = "";
-
-    public int? CustomerCategoryId { get; set; }
-    public CustomerCategory? CustomerCategory { get; set; }
-}
-
-public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
-{
-    public void Configure(EntityTypeBuilder<Customer> builder)
-    {
-        builder.HasKey(s => s.Id);
-        builder.Property(s => s.Name).IsRequired().HasMaxLength(100);
-        builder.Property(s => s.Address).HasMaxLength(200);
-        builder.Property(s => s.Email).HasMaxLength(50);
-        builder.Property(s => s.Phone).HasMaxLength(20);
-        builder.Property(s => s.Iban).HasMaxLength(34);
-        builder.HasOne(q => q.CustomerCategory).WithMany().HasForeignKey(q => q.CustomerCategoryId);
-    }
-}
-
-class CustomerSeeding : SeedEntity<BackendContext, Customer>
-{
-    readonly List<int?> CustomerCategoryIdList;
-
-    public CustomerSeeding(BackendContext context) : base(context)
-    {
-        CustomerCategoryIdList = context.CustomerCategories.Select(q => (int?)q.Id).ToList();
-        CustomerCategoryIdList.Add(null);
-    }
-
-    protected override IEnumerable<Customer> GetSeedItems()
-    {
-        for (var i = 0; i < 500; i++)
-        {
-            var faker = new Faker("it");
-            yield return new Customer
-            {
-                Name = faker.Company.CompanyName(),
-                Address = faker.Address.FullAddress(),
-                Email = faker.Internet.Email(),
-                Phone = faker.Phone.PhoneNumber(),
-                Iban = faker.Finance.Iban(),
-                CustomerCategoryId = faker.PickRandom(CustomerCategoryIdList),
-            };
-        }
-    }
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string Iban { get; set; } = string.Empty;
+    public string CategoryCode { get; set; } = string.Empty;
+    public string CategoryDescription { get; set; } = string.Empty;
 }
