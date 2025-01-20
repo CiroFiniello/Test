e15a1e9cae9386c4e7c327fb77a6faf443464292
Ciro <162831871+CiroFiniello@users.noreply.github.com>
2025-01-20 16:35:54 +0000

    Ho eseguito modifiche al file "RouteRegistrationExtensions.cs" Ho implementato la minimal Api di Customers

--- src/Backend/RouteRegistrationExtensions.cs
+++ src/Backend/RouteRegistrationExtensions.cs
@@ -1,5 +1,6 @@
 using Backend.Features.Employees;
 using Backend.Features.Suppliers;
+using Backend.Features.Customers;
 
 namespace Backend;
 
@@ -16,5 +17,8 @@ static class RouteRegistrationExtensions
         apiGroup.MapGet("employees/list", async ([AsParameters] EmployeesListQuery query, IMediator mediator) => await mediator.Send(query))
                     .WithName("GetEmployeesList")
                     .WithOpenApi();
+    apiGroup.MapGet("customers/list", async ([AsParameters] CustomersListQuery query, IMediator mediator) => await mediator.Send(query))
+            .WithName("GetCustomersList")
+            .WithOpenApi();
     }
 }
